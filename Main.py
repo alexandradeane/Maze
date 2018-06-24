04e947f9a21711717f3a9b8f14570bd4b70a13e3
Alexandra Deane <alexandradeane1@gmail.com>
2018-06-23 19:13:55 -0700

    Comments on what we have to do

---
+++ Main.py
@@ -0,0 +1,18 @@
+# Coded by Alex, Cord, Cobey
+# Runs in Python 3.6
+
+# This script will contain main gameplay loop
+# Needs to be written
+
+# Alex:
+# Another script for Maze generation 15x15 2D lists (return initial position and goal as well)
+
+# Cord:
+# user input function asking user and returning a move in ["up", "down", "left", "right"]
+
+# Cobey:
+# Need function to update the status of position in the maze based on the move
+
+
+# Initialization function that returns state of maze with initial position and goal
+# Function to print the current maze (with position and goal)
